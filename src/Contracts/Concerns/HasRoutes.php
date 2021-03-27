ebe34d4625d6a0e4201c9603a2f2833a0371d2a2
Marek Gogoľ <info@marekgogol.sk>
2021-03-27 19:36:42 +0100

    updated cart routes

--- src/Contracts/Concerns/HasRoutes.php
+++ src/Contracts/Concerns/HasRoutes.php
@@ -8,38 +8,38 @@ trait HasRoutes
 {
     public function routesForProducts()
     {
-        Route::group(['namespace' => '\AdminEshop\Controllers\Cart'], function(){
-            Route::post('/cart/add', 'CartController@addItem')->name('cart::addItem')->visible();
-            Route::post('/cart/update', 'CartController@updateQuantity')->name('cart::updateQuantity')->visible();
-            Route::post('/cart/remove', 'CartController@removeItem')->name('cart::removeItem')->visible();
+        Route::group(['namespace' => '\AdminEshop\Controllers'], function(){
+            Route::post('/cart/add', 'Cart\CartController@addItem')->name('cart::addItem')->visible();
+            Route::post('/cart/update', 'Cart\CartController@updateQuantity')->name('cart::updateQuantity')->visible();
+            Route::post('/cart/remove', 'Cart\CartController@removeItem')->name('cart::removeItem')->visible();
         });
     }
 
     public function routesForDiscounts()
     {
-        Route::group(['namespace' => '\AdminEshop\Controllers\Cart'], function(){
-            Route::post('/cart/discount/add', 'CartController@addDiscountCode')->name('cart::addDiscountCode')->visible();
-            Route::post('/cart/discount/remove', 'CartController@removeDiscountCode')->name('cart::removeDiscountCode')->visible();
+        Route::group(['namespace' => '\AdminEshop\Controllers'], function(){
+            Route::post('/cart/discount/add', 'Cart\CartController@addDiscountCode')->name('cart::addDiscountCode')->visible();
+            Route::post('/cart/discount/remove', 'Cart\CartController@removeDiscountCode')->name('cart::removeDiscountCode')->visible();
         });
     }
 
     public function routesForCart()
     {
-        Route::group(['namespace' => '\AdminEshop\Controllers\Cart'], function(){
-            Route::get('/cart/fullSummary', 'CartController@getFullSummary')->name('cart::fullSummary')->visible();
-            Route::get('/cart/delivery-locations/{id}', 'CartController@getDeliveryLocations')->name('cart::getDeliveryLocations')->visible();
+        Route::group(['namespace' => '\AdminEshop\Controllers'], function(){
+            Route::get('/cart/fullSummary', 'Cart\CartController@getFullSummary')->name('cart::fullSummary')->visible();
+            Route::get('/cart/delivery-locations/{id}', 'Cart\CartController@getDeliveryLocations')->name('cart::getDeliveryLocations')->visible();
 
-            Route::post('/cart/setDelivery', 'CartController@setDelivery')->name('cart::setDelivery')->visible();
-            Route::post('/cart/setPaymentMethod', 'CartController@setPaymentMethod')->name('cart::setPaymentMethod')->visible();
-            Route::post('/cart/setCountry', 'CartController@setCountry')->name('cart::setCountry')->visible();
+            Route::post('/cart/setDelivery', 'Cart\CartController@setDelivery')->name('cart::setDelivery')->visible();
+            Route::post('/cart/setPaymentMethod', 'Cart\CartController@setPaymentMethod')->name('cart::setPaymentMethod')->visible();
+            Route::post('/cart/setCountry', 'Cart\CartController@setCountry')->name('cart::setCountry')->visible();
         });
     }
 
     public function routesForCartSubmit()
     {
-        Route::group(['namespace' => '\AdminEshop\Controllers\Cart'], function(){
-            Route::post('/cart/submit', 'CartController@submitOrder')->visible();
-            Route::get('/cart/success/{id?}/{orderhash?}', 'CartController@success')->visible();
+        Route::group(['namespace' => '\AdminEshop\Controllers'], function(){
+            Route::post('/cart/submit', 'Cart\CartController@submitOrder')->visible();
+            Route::get('/cart/success/{id?}/{orderhash?}', 'Cart\CartController@success')->visible();
         });
     }
 
