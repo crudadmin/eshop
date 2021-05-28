ba63a7dfb96386307766e650abd5c04a865675e5
Marek Gogoľ <info@marekgogol.sk>
2021-05-28 10:24:23 +0200

    fix

--- src/Views/mail/order/received.blade.php
+++ src/Views/mail/order/received.blade.php
@@ -7,15 +7,17 @@
 
 @include('admineshop::mail.order.slots.items')
 
-@include('admineshop::mail.order.slots.received_before')
+@include('admineshop::mail.order.slots.slot_before')
 
 @include('admineshop::mail.order.slots.info')
 
 @include('admineshop::mail.order.slots.delivery')
 
-@include('admineshop::mail.order.slots.received_after')
+@include('admineshop::mail.order.slots.slot_middle')
 
 @include('admineshop::mail.order.slots.additional_info')
+
+@include('admineshop::mail.order.slots.slot_after')
 
 @include('admineshop::mail.order.slots.action_button')
 
