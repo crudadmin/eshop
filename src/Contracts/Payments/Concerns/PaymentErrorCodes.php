b175a20b5449277a24abf59cd23d597bc3def75c
Marek Gogoľ <info@marekgogol.sk>
2021-05-30 10:53:11 +0200

    fixes

--- src/Contracts/Payments/Concerns/PaymentErrorCodes.php
+++ src/Contracts/Payments/Concerns/PaymentErrorCodes.php
@@ -20,7 +20,7 @@ class PaymentErrorCodes
     static function getMessages()
     {
         return self::$messages + [
-            self::CODE_ERROR => _('Nastala nečakaná chyba pri spracovani platby. Skúste platbu vykonať neskôr neskôr, alebo nás prosím kontaktujte.'),
+            self::CODE_ERROR => _('Nastala nečakaná chyba pri spracovani platby. Skúste platbu vykonať neskôr, alebo nás prosím kontaktujte.'),
             self::CODE_PAYMENT_UNVERIFIED => _('Vaša objednávka bola úspešne zaznamenaná, no potvrdenie Vašej platby sme zatiaľ neobdržali. V prípade ak ste platbu nevykonali, môžete ju uhradiť opätovne z emailu, alebo nás kontaktujte pre ďalšie informácie.'),
             self::CODE_PAID => _('Vaša objednávka už bola úspešne zaplatená.'),
         ];
