c07dbaa50c8de4a5eaea46c81b3b7777cdb75c1e
Marek Gogoľ <info@marekgogol.sk>
2021-09-08 15:03:13 +0200

    added heureka cache

--- src/Controllers/Store/HeurekaController.php
+++ src/Controllers/Store/HeurekaController.php
@@ -5,6 +5,7 @@ namespace AdminEshop\Controllers\Store;
 use AdminEshop\Contracts\Heureka\HeurekaBuilder;
 use AdminEshop\Controllers\Controller;
 use Admin;
+use Cache;
 
 class HeurekaController extends Controller
 {
@@ -12,11 +13,19 @@ class HeurekaController extends Controller
     {
         ini_set('max_execution_time', 300);
 
-        $builder = new HeurekaBuilder;
+        $callback = function(){
+            $builder = new HeurekaBuilder;
 
-        $deliveries = Admin::getModel('Delivery')->get();
+            $deliveries = Admin::getModel('Delivery')->get();
 
-        $xml = view('admineshop::xml.heureka', compact('builder', 'deliveries'))->render();
+            return view('admineshop::xml.heureka', compact('builder', 'deliveries'))->render();
+        };
+
+        if ( env('APP_DEBUG') == true ) {
+            $xml = $callback();
+        } else {
+            $xml = Cache::remember('heureka.feed', 600, $callback);
+        }
 
         return response($xml, 200, [
             'Content-Type' => 'application/xml'
