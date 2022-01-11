3ce7a00bbeaca9ccb7834d2f5e0c5b1af23f30ea
Marek Gogoľ <info@marekgogol.sk>
2022-01-11 19:41:54 +0100

    updates

--- src/Views/mail/order/slots/action_button.blade.php
+++ src/Views/mail/order/slots/action_button.blade.php
@@ -1,11 +1,22 @@
-@if ( $owner == false && $postPaymentUrl = $order->getPostPaymentUrl() )
-{{ _('Ak ste platbu nevykonali po vytvorení objednávky, môžete ju zaplatiť aj z tohto e-mailu.') }}
+@if ( $owner == false )
+    {{-- Payment action --}}
+    @if ( $postPaymentUrl = $order->getPostPaymentUrl() )
+        {{ _('Ak ste platbu nevykonali po vytvorení objednávky, môžete ju zaplatiť aj z tohto e-mailu.') }}
 
-@component('mail::button', ['url' => $postPaymentUrl ])
-{{ _('Zaplatiť platbu online') }}
-@endcomponent
-@else
-@component('mail::button', ['url' => env('APP_NUXT_URL') ?: url('/')])
-    {{ _('Pokračovať na eshop') }}
-@endcomponent
+        @component('mail::button', ['url' => $postPaymentUrl ])
+        {{ _('Zaplatiť platbu online') }}
+        @endcomponent
+
+    {{-- TODO: delivery data are not available in this email yet --}}
+    @elseif ( $order->deliveryTrackingUrl )
+        @component('mail::button', ['url' => $order->deliveryTrackingUrl])
+            {{ _('Sledovať objednávku') }}
+        @endcomponent
+
+    {{-- No action --}}
+    @else
+        @component('mail::button', ['url' => env('APP_NUXT_URL') ?: url('/')])
+            {{ _('Pokračovať na eshop') }}
+        @endcomponent
+    @endif
 @endif
--- src/Views/mail/order/status.blade.php
+++ src/Views/mail/order/status.blade.php
@@ -1,6 +1,7 @@
 @component('mail::message')
 # {{ sprintf(_('Dobrý deň %s'), $order->firstname) }},
 
+{{-- Status info --}}
 @if ( $order->status->email_content )
 @component('mail::panel')
 | {{ _('Informácia k stavu objednávky') }} |
@@ -9,11 +10,19 @@
 @endcomponent
 @endif
 
+{{-- Delivery info --}}
 @if ( $order->status->email_delivery && $order->delivery )
 @component('mail::panel')
 | {{ _('Informácie k doprave') }} |
 | :------------- |
 | {!! $order->delivery->description_email_status !!} |
+@endcomponent
+@endif
+
+{{-- Delivery tracking button --}}
+@if ( $order->deliveryTrackingUrl )
+@component('mail::button', ['url' => $order->deliveryTrackingUrl])
+    {{ _('Sledovať objednávku') }}
 @endcomponent
 @endif
 
