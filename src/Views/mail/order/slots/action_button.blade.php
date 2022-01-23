74bdf0752d88eeb364045f207a405f07e5d2536a
Marek Gogoľ <info@marekgogol.sk>
2022-01-23 10:46:01 +0100

    fixed email

--- src/Views/mail/order/slots/action_button.blade.php
+++ src/Views/mail/order/slots/action_button.blade.php
@@ -1,22 +1,22 @@
 @if ( $owner == false )
-    {{-- Payment action --}}
-    @if ( $postPaymentUrl = $order->getPostPaymentUrl() )
-        {{ _('Ak ste platbu nevykonali po vytvorení objednávky, môžete ju zaplatiť aj z tohto e-mailu.') }}
+{{-- Payment action --}}
+@if ( $postPaymentUrl = $order->getPostPaymentUrl() )
+{{ _('Ak ste platbu nevykonali po vytvorení objednávky, môžete ju zaplatiť aj z tohto e-mailu.') }}
 
-        @component('mail::button', ['url' => $postPaymentUrl ])
-        {{ _('Zaplatiť platbu online') }}
-        @endcomponent
+@component('mail::button', ['url' => $postPaymentUrl ])
+{{ _('Zaplatiť platbu online') }}
+@endcomponent
 
-    {{-- TODO: delivery data are not available in this email yet --}}
-    @elseif ( $order->deliveryTrackingUrl )
-        @component('mail::button', ['url' => $order->deliveryTrackingUrl])
-            {{ _('Sledovať objednávku') }}
-        @endcomponent
+{{-- TODO: delivery data are not available in this email yet --}}
+@elseif ( $order->deliveryTrackingUrl )
+@component('mail::button', ['url' => $order->deliveryTrackingUrl])
+{{ _('Sledovať objednávku') }}
+@endcomponent
 
-    {{-- No action --}}
-    @else
-        @component('mail::button', ['url' => env('APP_NUXT_URL') ?: url('/')])
-            {{ _('Pokračovať na eshop') }}
-        @endcomponent
-    @endif
+{{-- No action --}}
+@else
+@component('mail::button', ['url' => env('APP_NUXT_URL') ?: url('/')])
+{{ _('Pokračovať na eshop') }}
+@endcomponent
+@endif
 @endif
