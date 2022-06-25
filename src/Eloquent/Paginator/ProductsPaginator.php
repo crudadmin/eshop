1b1e598b6ca7c00059e3cace3758679790a9b697
Marek Gogoľ <info@marekgogol.sk>
2022-06-25 14:37:05 +0200

    products paginator improved

--- src/Eloquent/Paginator/ProductsPaginator.php
+++ src/Eloquent/Paginator/ProductsPaginator.php
@@ -2,6 +2,7 @@
 
 namespace AdminEshop\Eloquent\Paginator;
 
+use AdminEshop\Contracts\Listing\UrlWindow;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Store;
 
@@ -9,12 +10,30 @@ class ProductsPaginator extends LengthAwarePaginator
 {
     protected $pushIntoArray = [];
 
-    public $onEachSide = 1;
+    public $onEachSide = 2;
 
     public function toArray()
     {
         $array = parent::toArray();
 
         return array_merge($array, $this->pushIntoArray);
+    }
+
+    /**
+     * Get the array of elements to pass to the view.
+     *
+     * @return array
+     */
+    protected function elements()
+    {
+        $window = UrlWindow::make($this);
+
+        return array_filter([
+            $window['first'],
+            is_array($window['slider']) ? '...' : null,
+            $window['slider'],
+            is_array($window['last']) ? '...' : null,
+            $window['last'],
+        ]);
     }
 }
