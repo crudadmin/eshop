b74806df104ace718bda570beccae9e5dfbd6dfd
Marek Gogoľ <info@marekgogol.sk>
2022-12-08 13:09:21 +0100

    fixed decimal places

--- src/Models/Orders/OrdersItem.php
+++ src/Models/Orders/OrdersItem.php
@@ -98,10 +98,10 @@ class OrdersItem extends AdminModel implements UsesIdentifier, DiscountSupport
                 'order_item' => 'name:Patrí k položke|belongsTo:orders_items,id|inaccessible',
             ])->id('itemAdditional'),
             Group::fields([
-                'default_price' => 'name:Pôvodna cena bez DPH|invisible|type:decimal|title:Cena produktu v čase objednania.|disabled',
-                'price' => 'name:Cena/j bez DPH|type:decimal|required_if:manual_price,1|disabledIf:manual_price,0',
+                'default_price' => 'name:Pôvodna cena bez DPH|invisible|type:decimal|decimal_length:'.config('admineshop.prices.decimals_places').'|title:Cena produktu v čase objednania.|disabled',
+                'price' => 'name:Cena/j bez DPH|type:decimal|decimal_length:'.config('admineshop.prices.decimals_places').'|required_if:manual_price,1|disabledIf:manual_price,0',
                 'vat' => 'name:DPH %|type:select|default:'.Store::getDefaultVat().'|required_if:manual_price,1|disabledIf:manual_price,0',
-                'price_vat' => 'name:Cena/j s DPH|type:decimal|required_if:manual_price,1|disabledIf:manual_price,0',
+                'price_vat' => 'name:Cena/j s DPH|type:decimal|required_if:manual_price,1|decimal_length:'.config('admineshop.prices.decimals_places').'|disabledIf:manual_price,0',
                 'manual_price' => 'name:Manuálna cena|default:0|hidden|tooltip:Ak je manuálna cena zapnutá, nebude na cenu pôsobiť žiadna automatická zľava.|type:checkbox',
                 'discountable' => 'name:Povoliť zľavy na položku|type:checkbox|default:0|invisible',
             ])->inline()
