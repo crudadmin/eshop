31e729cfee9aa221226ead96bf1c128e2840a8ec
Marek Gogoľ <info@marekgogol.sk>
2023-05-01 19:30:07 +0200

    payments refactor

--- src/Models/Orders/Payment.php
+++ src/Models/Orders/Payment.php
@@ -2,22 +2,10 @@
 
 namespace AdminEshop\Models\Orders;
 
-use AdminPayments\Contracts\Concerns\Orderable;
 use AdminPayments\Models\Payments\Payment as BasePayment;
 
+/**
+ * Legacy model
+ */
 class Payment extends BasePayment
-{
-    public function mutateFields($fields)
-    {
-        parent::mutateFields($fields);
-
-        $fields->pushBefore([
-            'order' => 'name:Objednavka|belongsTo:orders,name',
-        ]);
-    }
-
-    public function getOrder() : Orderable
-    {
-        return $this->order;
-    }
-}
+{}
