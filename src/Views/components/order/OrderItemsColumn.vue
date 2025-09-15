757f5022a8e1f34b372eb6e9d9043579955101ef
Marek Gogol <info@marekgogol.sk>
2025-09-15 10:15:56 +0200

    ref

--- src/Views/components/order/OrderItemsColumn.vue
+++ src/Views/components/order/OrderItemsColumn.vue
@@ -1,5 +1,5 @@
 <template>
-<div @mouseenter="loadProducts" ref="element" data-template='<div class="tooltip tooltip-products" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>' data-toggle="tooltip" data-html="true" :title="products" :key="loaded ? 1 : 0">
+<div @mouseenter="loadProducts" ref="element" data-template='<div class="tooltip tooltip-products" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>' data-toggle="tooltip" data-bs-html="true" :title="products" :key="loaded ? 1 : 0">
     {{ valueMutated }}
 </div>
 </template>
@@ -21,11 +21,11 @@ export default {
                 return;
             }
 
-            let response = await this.$http.get('/admin/orders/'+this.row.id+'/items');
+            let response = await useAxios().$get('/orders/'+this.row.id+'/items');
 
             $(this.$refs.element).tooltip('hide');
 
-            this.products = response.data.map(item => {
+            this.products = response.map(item => {
                 return `
                 <div class="orderItemTooltip">
                     <strong class="--quantity">${item.quantity}x </strong>
