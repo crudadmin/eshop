8cb67a6b0751c4c66e901c58eb47f0e5edcc4aa3
Marek Gogol <info@marekgogol.sk>
2025-10-23 20:24:00 +0200

    fix

--- src/Controllers/Feed/FeedController.php
+++ src/Controllers/Feed/FeedController.php
@@ -2,13 +2,16 @@
 
 namespace AdminEshop\Controllers\Feed;
 
-use AdminEshop\Contracts\Feed\FeedGenerator;
+use Admin\Eloquent\AdminModel;
 use AdminEshop\Controllers\Controller;
+use AdminEshop\Contracts\Feed\FeedGenerator;
 
 class FeedController extends Controller
 {
     public function index($type = 'heureka')
     {
+        AdminModel::$localizedResponseArray = false;
+
         return (new FeedGenerator($type, request('locale'), request('currency')))->response();
     }
 }
