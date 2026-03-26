2d303ef8b2bb6e4c07d8bea15c2da56cdcbc532b
Marek Gogol <info@marekgogol.sk>
2026-03-26 08:31:00 +0100

    ref

--- src/Models/Store/SynchronizerReport.php
+++ src/Models/Store/SynchronizerReport.php
@@ -122,7 +122,7 @@ class SynchronizerReport extends AdminModel
         if ( $removeOlderReportsFrom ) {
             $this->where('id', '<', $removeOlderReportsFrom->getKey())
                  ->where('name', $name)
-                 ->delete();
+                 ->forceDelete();
         }
     }
 }
